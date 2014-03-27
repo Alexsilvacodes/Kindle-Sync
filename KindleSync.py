296ba36e76ce73434f400717ac85e9e3b4101669
Alejandro Silva Fernández <h4ll0ck@gmail.com>
2014-03-27 22:47:50 +0100

    Interface changes, initialization and other changes
    -Added initializing function
    -Changed interface to a cooler one :D
    -New images to tag epub and mobi files
    -Adding conversion functionality

--- KindleSync.py
+++ KindleSync.py
@@ -2,20 +2,33 @@
 # -*- coding: utf-8 -*-
 
 from biplist import *
-from os.path import expanduser
+from os.path import *
+from os import mkdir
 
 # Globals
 debug = False
+home = expanduser("~")
+# if MacOS X
+ks_folder = home + "/Library/Application Support/Kindle Sync"
+ibooks_folder = home + "/Library/Containers/com.apple.BKAgentService/Data/Documents/iBooks/Books/"
+# if Linux
+# ks_folder = home + ".Kindle Sync"
 
-#def 
+#
+# Definition of folder an config files
+#
+def initFolderConf():
+	try:
+		mkdir(ks_folder)
+		mkdir(ks_folder + "/ConvertedKindle")
+	except Exception, e:
+		print e
 
 #
 # Collect epub from iBooks Library to convert it to .mobi files
 #
 def collectiBooks():
-	home = expanduser("~")
-
-	books_library = readPlist(home + "/Library/Containers/com.apple.BKAgentService/Data/Documents/iBooks/Books/Books.plist")
+	books_library = readPlist(ibooks_folder + "Books.plist")
 
 	books = []
 
@@ -23,7 +36,19 @@ def collectiBooks():
 		if "epub" in elem['path']:
 			epub_file = elem['path'].split("/")[-1]
 			epub_name = elem['itemName'].encode('utf-8')
-			books.append({'book_name': epub_name, 'book_file': epub_file})
+			# {book_name: "blabla", book_file: "blabla.epub", converted: True}
+			books.append({'book_name': epub_name, 'book_file': epub_file, 'converted': isfile(ks_folder + "/ConvertedKindle/" + epub_file.split(".")[0] + ".mobi")})
 			if debug: print "EPUB: ", epub_name, " File: ", epub_file
 
 	return books
+
+#
+# Creates Library.plist file
+#
+def createLibrary(books):
+	try:
+		writePlist(books, ks_folder + "/Library.plist")
+	except Exception, e:
+		print e
+
+if debug: collectiBooks()
--- KindleSyncMain.py
+++ KindleSyncMain.py
@@ -2,12 +2,13 @@
 
 # Form implementation generated from reading ui file 'KindleSyncMain.ui'
 #
-# Created: Thu Mar 27 03:39:27 2014
+# Created: Thu Mar 27 15:40:47 2014
 #      by: PyQt4 UI code generator 4.10.3
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
+from KindleSync import *
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -23,15 +24,76 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
+class MyTableView(QtGui.QTableView):
+    def hola():
+        pass
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(687, 535)
+        MainWindow.resize(684, 520)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setMinimumSize(QtCore.QSize(684, 520))
+        MainWindow.setMaximumSize(QtCore.QSize(684, 520))
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.tableViewLibrary = QtGui.QTableView(self.centralwidget)
-        self.tableViewLibrary.setGeometry(QtCore.QRect(10, 10, 661, 391))
+        self.tableViewLibrary = MyTableView(self.centralwidget)
+        self.tableViewLibrary.setGeometry(QtCore.QRect(0, 0, 684, 401))
+        self.tableViewLibrary.setShowGrid(False)
+        self.tableViewLibrary.setCornerButtonEnabled(False)
+        self.tableViewLibrary.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
         self.tableViewLibrary.setObjectName(_fromUtf8("tableViewLibrary"))
+        self.tableViewLibrary.horizontalHeader().setVisible(False)
+        self.tableViewLibrary.verticalHeader().setVisible(False)
+
+        ## Changes ##
+        initFolderConf()
+        self.books = collectiBooks()
+        createLibrary(self.books)
+        if debug: self.books = [{'book_name': "1"},{'book_name': "2"},{'book_name': "3"}]
+        self.model = QtGui.QStandardItemModel(len(self.books), 2)
+
+        # # Section Header List
+        # headerItem = QtGui.QStandardItem()
+        # headerItem.setText("Libros")
+        # headerItem.setSelectable(False)
+        # self.model.setHorizontalHeaderItem(0, headerItem)
+        self.tableViewLibrary.setModel(self.model)
+        
+        blue_color = QtGui.QBrush(QtGui.QColor(240, 243, 249))
+        epub_icon = QtGui.QImage(_fromUtf8("images/epub_tag.png"))
+        mobi_icon = QtGui.QImage(_fromUtf8("images/mobi_tag.png"))
+        # epub_icon = epub_icon.scaled(54, 15)
+        for i,book in enumerate(self.books):
+            itemCol1 = QtGui.QStandardItem(_fromUtf8(book['book_name']))
+            itemCol2 = QtGui.QStandardItem()
+            itemCol1.setToolTip(_fromUtf8(book['book_name']))
+            itemCol2.setToolTip(_fromUtf8(book['book_name']))
+            itemCol1.setEditable(False)
+            itemCol2.setEditable(False)
+
+            if book['converted']:
+                itemCol2.setData(QtCore.QVariant(mobi_icon), QtCore.Qt.DecorationRole)
+            else:
+                itemCol2.setData(QtCore.QVariant(epub_icon), QtCore.Qt.DecorationRole)
+            if not i%2: 
+                itemCol1.setBackground(blue_color)
+                itemCol2.setBackground(blue_color)
+            self.model.setItem(i, 0, itemCol1)
+            self.model.setItem(i, 1, itemCol2)
+        ## Slot Signal >> QtCore.QObject.connect(self.tableViewLibrary, QtCore.SIGNAL("entered(const QModelIndex&)"), self.onClickConvertSel)
+        if len(self.books) > 13:
+            tableViewWidth = self.tableViewLibrary.geometry().width()-17
+        else:
+            tableViewWidth = self.tableViewLibrary.geometry().width()-2
+        self.tableViewLibrary.setColumnWidth(0, tableViewWidth * 0.90)
+        self.tableViewLibrary.setColumnWidth(1, tableViewWidth * 0.10)
+        ##############
+
         self.btnEpMoAll = QtGui.QPushButton(self.centralwidget)
         self.btnEpMoAll.setGeometry(QtCore.QRect(10, 400, 151, 91))
         self.btnEpMoAll.setText(_fromUtf8(""))
@@ -40,9 +102,7 @@ class Ui_MainWindow(object):
         self.btnEpMoAll.setIcon(icon)
         self.btnEpMoAll.setIconSize(QtCore.QSize(128, 64))
         self.btnEpMoAll.setCheckable(False)
-        self.btnEpMoAll.setAutoDefault(False)
-        self.btnEpMoAll.setDefault(True)
-        self.btnEpMoAll.setFlat(False)
+        self.btnEpMoAll.setFlat(True)
         self.btnEpMoAll.setObjectName(_fromUtf8("btnEpMoAll"))
         self.btnEpMoSel = QtGui.QPushButton(self.centralwidget)
         self.btnEpMoSel.setGeometry(QtCore.QRect(160, 400, 151, 91))
@@ -52,9 +112,7 @@ class Ui_MainWindow(object):
         self.btnEpMoSel.setIcon(icon1)
         self.btnEpMoSel.setIconSize(QtCore.QSize(128, 64))
         self.btnEpMoSel.setCheckable(False)
-        self.btnEpMoSel.setAutoDefault(False)
-        self.btnEpMoSel.setDefault(True)
-        self.btnEpMoSel.setFlat(False)
+        self.btnEpMoSel.setFlat(True)
         self.btnEpMoSel.setObjectName(_fromUtf8("btnEpMoSel"))
         self.btnUploadSel = QtGui.QPushButton(self.centralwidget)
         self.btnUploadSel.setGeometry(QtCore.QRect(520, 400, 151, 91))
@@ -64,9 +122,7 @@ class Ui_MainWindow(object):
         self.btnUploadSel.setIcon(icon2)
         self.btnUploadSel.setIconSize(QtCore.QSize(128, 64))
         self.btnUploadSel.setCheckable(False)
-        self.btnUploadSel.setAutoDefault(False)
-        self.btnUploadSel.setDefault(True)
-        self.btnUploadSel.setFlat(False)
+        self.btnUploadSel.setFlat(True)
         self.btnUploadSel.setObjectName(_fromUtf8("btnUploadSel"))
         self.btnUploadAll = QtGui.QPushButton(self.centralwidget)
         self.btnUploadAll.setGeometry(QtCore.QRect(370, 400, 151, 91))
@@ -76,17 +132,22 @@ class Ui_MainWindow(object):
         self.btnUploadAll.setIcon(icon3)
         self.btnUploadAll.setIconSize(QtCore.QSize(128, 64))
         self.btnUploadAll.setCheckable(False)
-        self.btnUploadAll.setAutoDefault(False)
-        self.btnUploadAll.setDefault(True)
-        self.btnUploadAll.setFlat(False)
+        self.btnUploadAll.setFlat(True)
         self.btnUploadAll.setObjectName(_fromUtf8("btnUploadAll"))
+
+        self.btnEpMoAll.clicked.connect(self.onClickConvertAll)
+        self.btnEpMoSel.clicked.connect(self.onClickConvertSel)
+        self.btnUploadAll.clicked.connect(self.onClickSendAll)
+        self.btnUploadSel.clicked.connect(self.onClickSendSel)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 687, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 684, 22))
         self.menubar.setObjectName(_fromUtf8("menubar"))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(MainWindow)
         self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        self.statusbar.showMessage(str(len(self.books)) + " libros en tu biblioteca", 0)
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
@@ -95,7 +156,21 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "Kindle Sync", None))
 
-import KindleConvert_rc
+    def onClickConvertSel(self):
+        index_list = self.tableViewLibrary.selectionModel().selectedIndexes()
+        print self.model.item(1).sizeHint()
+
+        for index in index_list:
+            print index.row()
+
+    def onClickConvertAll(self):
+        pass
+
+    def onClickSendAll(self):
+        pass
+
+    def onClickSendSel(self):
+        pass
 
 if __name__ == "__main__":
     import sys
